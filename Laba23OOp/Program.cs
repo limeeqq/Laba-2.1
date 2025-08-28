793e02827fdc8ef6ffd819a70efeb10529991a0b
loligoty@gmail.com <loligoty@gmail.com>
2025-08-28 08:22:24 +0300

    Add project files.

---
+++ Laba23OOp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace GeometryApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Segment s = new Segment(0, 0, 3, 4);
+
+            Console.WriteLine(s.ToString());
+            Console.WriteLine($"Length = {s.Length():F2}");
+            Console.WriteLine($"Angle with OX = {s.AngleOX():F2} degrees");
+        }
+    }
+}
---
+++ Laba23OOp/Segment.cs
@@ -0,0 +1,23 @@
+﻿
+public class Segment : Line
+{
+    public Segment() : base() { }
+
+    public Segment(double x1, double y1, double x2, double y2)
+        : base(x1, y1, x2, y2) { }
+
+    public Segment(Segment other) : base(other) { }
+
+    public double AngleOX()
+    {
+        double dx = X2 - X1;
+        double dy = Y2 - Y1;
+        return Math.Atan2(dy, dx) * 180.0 / Math.PI;
+    }
+
+    public override string ToString()
+    {
+        return $"Segment: ({X1},{Y1}) -> ({X2},{Y2})";
+    }
+}
+
